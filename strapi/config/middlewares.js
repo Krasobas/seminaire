a24461af21d68a97325f5adfce9976a5db672994
v.krasov.ext <v.krasov@aleria.cloud>
2026-05-12 19:01:18 +0400

    fix strapi

--- strapi/config/middlewares.js
+++ strapi/config/middlewares.js
@@ -5,7 +5,6 @@ module.exports = ({ env }) => [
   {
     name: 'strapi::cors',
     config: {
-      enabled: true,
       origin: [env('FRONTEND_URL', 'http://localhost:4321'), 'https://seminaire.krasobas.com', 'https://seminaire-orthodoxe.fr'],
       headers: ['Content-Type', 'Authorization', 'Accept'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
